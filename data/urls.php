97cfc8e13abfbe37b8538964ada480b67f538821
borislav <b.manolov@gmail.com>
2020-10-31 23:04:55 +0100

    add last data urls

--- data/urls.php
+++ data/urls.php
@@ -3,7 +3,9 @@ $files = [
 	__DIR__.'/url_constitution.php',
 	__DIR__.'/url_code.php',
 	__DIR__.'/url_laws.php',
-	//__DIR__.'/url_ords.php',
+	__DIR__.'/url_ords.php',
+	__DIR__.'/url_regs.php',
+	__DIR__.'/url_reg_laws.php',
 ];
 return array_merge(...array_map(function(string $file) {
 	return require $file;
